63e44ab544735b53983362eb62ca52fbf8779207
Vatsal Sood <vatsalsood@dal.ca>
2020-08-21 15:39:02 -0300

    pagination

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, Fragment } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { getRows, getColumns } from "./processapi";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles,withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -10,16 +10,28 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import TablePagination from "@material-ui/core/TablePagination";
 
 const App = () => {
   const [rows, setRows] = useState([]);
   const [columns, setColumns] = useState({});
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     setRows(getRows());
     setColumns(getColumns());
   }, []);
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  
   const useStyles = makeStyles({
     table: {
       minWidth: 650,
@@ -27,37 +39,67 @@ const App = () => {
   });
 
   const classes = useStyles();
+  const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>{columns.id}</TableCell>
-            <TableCell>{columns.name}</TableCell>
-            <TableCell align="right">{columns.nickname}</TableCell>
-            <TableCell align="right">{columns.conference}</TableCell>
-            <TableCell align="right">{columns.division}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.nickname}</TableCell>
-              <TableCell align="right">{row.conference}</TableCell>
-              <TableCell align="right">{row.division}</TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>{columns.id}</StyledTableCell>
+              <StyledTableCell>{columns.name}</StyledTableCell>
+              <StyledTableCell align="right">{columns.nickname}</StyledTableCell>
+              <StyledTableCell align="right">{columns.conference}</StyledTableCell>
+              <StyledTableCell align="right">{columns.division}</StyledTableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <button></button>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+             {/* Slice the page to show maximum number of rows for pagination */}
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.id}
+                  </StyledTableCell>
+                  <StyledTableCell component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.nickname}</StyledTableCell>
+                  <StyledTableCell align="right">{row.conference}</StyledTableCell>
+                  <StyledTableCell align="right">{row.division}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </div>
   );
 };
 
@@ -76,3 +118,12 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+/**
+ * Things done
+ * 1) render json into a table
+ * 2) pagination
+ * 3) Stylizing table
+ * 
+ */
