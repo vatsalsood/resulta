4272e8068632ed9832aa8d44bec9be7de345f771
Vatsal Sood <vatsalsood@dal.ca>
2020-08-21 11:44:38 -0300

    initial code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import getRows from "./processapi";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count);
+const App = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    setRows(getRows());
+  }, []);
+
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  const classes = useStyles();
 
   return (
-    <div>
-      Counter: {count}
-      <br />
-      <button onClick={() => { setCount(count + 1) }}>+</button>
-      <button onClick={() => { setCount(count - 1) }}>-</button>
-      <button onClick={() => { setCount(props.count) }}>Reset</button>
-    </div>);
-}
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.nickname}</TableCell>
+              <TableCell align="right">{row.conference}</TableCell>
+              <TableCell align="right">{row.division}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 App.defaultProps = {
-  count: 0
-}
-
+  count: 0,
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/processapi.js
@@ -0,0 +1,5 @@
+import myData from "./resulta.json";
+
+export default function getRows() {
+    return myData.results.data.team;
+}
